43c2122b1d5d216b68cb9e365667d6faf997d34d
guilherme-kaowan <gkaowan@id.uff.br>
2026-03-31 21:27:59 -0300

    Script com leitura da base de dados e com calculos da media

---
+++ imoveis-analise-media.R
@@ -0,0 +1,16 @@
+dados = read.csv("imoveis.csv", header = TRUE , sep = ';')
+mediapreco <- table(dados$preco>mean(dados$preco))
+tabela_media <- prop.table(mediapreco)
+tabela_media
+mediametragem <- table(dados$metragem>mean(dados$metragem))
+tabela_metragem<- prop.table(mediametragem)
+tabela_metragem
+mediaidade <- table(dados$idade>mean(dados$idade))
+tabela_idade <- prop.table(mediaidade)
+tabela_idade
+mediaitens <- table(dados$itens_embutidos>mean(dados$itens_embutidos))
+tabela_itens <- prop.table(mediaitens)
+tabela_itens
+mediaimposto <- table(dados$imposto_anual>mean(dados$imposto_anual))
+tabela_imposto <- prop.table(mediaimposto)
+tabela_imposto
