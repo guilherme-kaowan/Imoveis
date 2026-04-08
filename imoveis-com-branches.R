653c66a315db9debea04417755b905db17433e3e
guilherme-kaowan <gkaowan@id.uff.br>
2026-04-08 20:19:20 -0300

    Script com medidas

--- imoveis-com-branches.R
+++ imoveis-com-branches.R
@@ -3,4 +3,6 @@ dados = read.csv("imoveis.csv", header = TRUE, sep = ';')
 # 2. Graficos
 # Comando dos graficos entraram aqui
 # 3. Medidas
-# Comandos  das medidas entraram  aqui
+media <- mean(dados$imposto_anual, na.rm = TRUE)
+mediana <- median(dados$imposto_anual, na.rm = TRUE)
+desvio <- sd(dados$imposto_anual, na.rm = TRUE)
