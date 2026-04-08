3f109af321eba9023fc2dfd9796e487d3e626dea
guilherme-kaowan <gkaowan@id.uff.br>
2026-04-08 20:16:22 -0300

    Script com gráficos

--- imoveis-com-branches.R
+++ imoveis-com-branches.R
@@ -1,6 +1,6 @@
 #leitura do banco de dados
 dados = read.csv("imoveis.csv", header = TRUE, sep = ';')
 # 2. Graficos
-# Comando dos graficos entraram aqui
+hist(dados$metragem, main="Histograma da Metragem", xlab="Metragem")
 # 3. Medidas
 # Comandos  das medidas entraram  aqui
